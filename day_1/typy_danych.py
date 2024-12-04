4b6c9d12c607a094aa707d8ae7aa71edb3ad218b
Piotr <pmalafie@poczta.onet.pl>
2024-12-04 11:58:58 +0100

    Typy danych podstawowe info

--- day_1/typy_danych.py
+++ day_1/typy_danych.py
@@ -1,17 +1,25 @@
 import sys
 
+# ctrl + alt + l - formatuje do POP8
+
 print("Tekst")
 print('drugi tekst')
-#komentarzz
-#ctrl / - komentarz
+# komentarzz
+# ctrl / - komentarz
 print(type('Typ_String'))
 
 print(39)
 print(type(39))
 print(sys.int_info)
 
-#podpowiedzi typów jest falka jeżeli coś nie się nie zgadza
+# podpowiedzi typów jest falka jeżeli coś nie się nie zgadza
 name: str = "Tomek"
-print(name) # Tomek
+print(name)  # Tomek
 name = 90
-print (name) # 90
+print(name)  # 90
+
+print(0.1 + 0.5)  # 0.6
+print(0.1 + 0.7)  # 0.7999999999999999
+print(0.1 + 0.2)  # 0.30000000000000004
+
+# decimal - typ danych do przechowywania pięniędzy, omija problem zaokrąglaniaTYpy
