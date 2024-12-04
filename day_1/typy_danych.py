edce3cd2e387074476a58e8cfc5c9191f22bafa8
Piotr <pmalafie@poczta.onet.pl>
2024-12-04 11:35:31 +0100

    Init

---
+++ day_1/typy_danych.py
@@ -0,0 +1,17 @@
+import sys
+
+print("Tekst")
+print('drugi tekst')
+#komentarzz
+#ctrl / - komentarz
+print(type('Typ_String'))
+
+print(39)
+print(type(39))
+print(sys.int_info)
+
+#podpowiedzi typów jest falka jeżeli coś nie się nie zgadza
+name: str = "Tomek"
+print(name) # Tomek
+name = 90
+print (name) # 90
