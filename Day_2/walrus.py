1d4738f780a8e22800d41e2c357a5b68bf35e294
Piotr <pmalafie@poczta.onet.pl>
2024-12-05 15:16:08 +0100

    [1.8] użycie walrus

---
+++ Day_2/walrus.py
@@ -0,0 +1,20 @@
+# walrus operator -> operator morsa
+# :=
+
+name = "Radek"
+if name == "Radek":
+    print("Radek")
+
+if name := "Radek":
+    print("Radek")
+
+
+tekst = "Radek Radek"
+a = len(tekst)
+if a > 3:
+    print(tekst, a)
+# Radek Radek 11
+
+if (a := len(tekst)) > 3:
+    print(tekst, a)
+# Radek Radek 11
