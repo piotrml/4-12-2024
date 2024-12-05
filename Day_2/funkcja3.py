04dfcc93478d805511734702ac17bbf1da43357e
Piotr <pmalafie@poczta.onet.pl>
2024-12-05 11:07:07 +0100

    [1.2] lambda - update

--- Day_2/funkcja3.py
+++ Day_2/funkcja3.py
@@ -9,5 +9,13 @@ def liczymy(x, y):
 print(liczymy(4, 7))  # 28
 
 liczymy2 = lambda x, y: x * y
-print(liczymy2(9, 7)) # 63
+print(liczymy2(9, 7))  # 63
 
+lista = [1, 2, 6, 15, 67, 90]
+lista_wyn = []
+for x in lista:
+    lista_wyn.append(x * 2)
+print(lista_wyn)  # [2, 4, 12, 30, 134, 180]
+
+print([i * 2 for i in lista])  # [2, 4, 12, 30, 134, 180]
+# moznenie_przez2 = lambada lista: for x in lista: x * 2
