32053349495b7129cf579c423ed56ca1de8f6d8b
Piotr <pmalafie@poczta.onet.pl>
2024-12-05 14:55:46 +0100

    [1.6] dekoratory i numpy

--- Day_2/funkcje8.py
+++ Day_2/funkcje8.py
@@ -1,5 +1,8 @@
 import time
 import tracemalloc
+from ctypes import HRESULT
+from turtledemo.penrose import start
+
 import numpy as np
 
 # pip install numpy
@@ -19,8 +22,8 @@ import numpy as np
 #Current memory usage: 76.29412841796875 MB
 #Peak memory usage: 76.29412841796875 MB
 # tracemalloc.start()
-# lista1 = list(range(10_000_000))
-# lista2 = list(range(10_000_000))
+lista1 = list(range(10_000_000))
+lista2 = list(range(10_000_000))
 # current, peak = tracemalloc.get_traced_memory()
 # tracemalloc.stop()
 # print(f'Current memory usage: {current / 1024 ** 2} MB')
@@ -30,3 +33,31 @@ import numpy as np
 
 array1 = np.arange(10_000_000)
 array2 = np.arange(10_000_000)
+
+def meas_time(func):
+    def wrapper(*args, **kwargs):
+        start_time =time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        exec_time = end_time - start_time
+        print(f'Czas wykoania funkcji {func.__name__}: {exec_time}')
+        return result
+    return wrapper
+
+@meas_time
+def add_without_np():
+    res = [lista1[i] +lista2[i] for i in range(len(lista1))]
+    return "OK"
+print("Start")
+print(add_without_np())
+
+@meas_time
+def add_np():
+    res = array1 + array2
+    return "OK np"
+print(add_np())
+# Start
+# Czas wykoania funkcji add_without_np: 1.7584948539733887
+# OK
+# Czas wykoania funkcji add_np: 0.053809404373168945
+# OK np
